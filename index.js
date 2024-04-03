039c7d8dbba77a1b87edcd99b0d936e26afaa6c8
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2024-04-03 11:09:14 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8080 });
+wss.on('connection', function connection(ws) {
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+    document.writeLine("data");
+  });
+
+  ws.send('something');
+});
